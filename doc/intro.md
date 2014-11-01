51cbe98f855302dad8b7cc2ec2eef9ce3043b239
SE Ngetha <ngetha@gmail.com>
2014-11-01 14:43:41 +0300

    Initial Commit
    New lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fasiri
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fasiri/core_test.clj
@@ -0,0 +1,7 @@
+(ns fasiri.core-test
+  (:require [clojure.test :refer :all]
+            [fasiri.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
